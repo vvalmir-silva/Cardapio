568b26ddcde2d22a9ec344858e8c01d1b7d56e25
Valmir Virginio da Silva <vvalmir.devsilva@gmail.com>
2024-09-23 16:34:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ checkoutBtn.addEventListener("click", function(){
         )
     }).join("")
     const messege = encodeURIComponent(cartItems)
-    const phone = "11960814357"
+    const phone = "5511960814357"
 
     window.open(`https://wa.me/${phone}?text=${messege} Endereço: ${addressInput.value}`, "_blank")
 
